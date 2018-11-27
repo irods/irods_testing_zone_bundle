ac2230af74f4cb5c6087e363c79a42730de15044
Jaspreet Gill <jessi.k.gill@gmail.com>
2018-11-27 14:42:28 -0500

    add leak vms support for individual tests

--- jenkins_helper.py
+++ jenkins_helper.py
@@ -31,6 +31,7 @@ if __name__ == '__main__':
     parser.add_argument('--use_ssl', action='store_true')
     parser.add_argument('--use_mungefs', action='store_true')
     parser.add_argument('--upgrade_test', nargs='+')
+    parser.add_argument('--leak_vms', type=library.make_argparse_true_or_false('--leak_vms'), required=False)
     parser.add_argument('--output_directory', type=str, required=True)
     args = parser.parse_args()
 
@@ -40,8 +41,9 @@ if __name__ == '__main__':
         zone_bundle = json.load(f)
 
     zone_bundle_output = os.path.join(args.output_directory, 'deployed_zone_bundle.json')
+ 
     deployed_zone_bundle = deploy.deploy(zone_bundle, args.deployment_name, version_to_packages_map, args.mungefs_packages_root_dir, zone_bundle_output)
-    with destroy.deployed_zone_bundle_manager(deployed_zone_bundle):
+    with destroy.deployed_zone_bundle_manager(deployed_zone_bundle, on_exception=not args.leak_vms, on_regular_exit=not args.leak_vms):
         if args.upgrade_test:
             for pd in args.upgrade_test:
                 upgrade.upgrade(deployed_zone_bundle, pd)
